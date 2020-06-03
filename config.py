a4d6359a1c5ceb2fb44f1b44fac5eef8aff49eec
SuPeRBoY321 <muso-azizbekov@mail.ru>
2020-06-03 21:48:53 +0300

    login

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Configuration(object):
 	DEBUG = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:q@localhost/my_db'
+	SECRET_KEY = '503015013694b169f37690a490978479653b'
--- views.py
+++ views.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
 from models import Post
+from login import RegistrationForm
+from login import LoginForm
 
 
 @app.route("/")
 def index():
 	return render_template('base.html') 
 
-
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template('login.html', title='Login', form=form)
