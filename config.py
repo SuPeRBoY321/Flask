2a67c647473b63344c51badbf9853db3326816db
SuPeRBoY321 <muso-azizbekov@mail.ru>
2020-06-05 02:06:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Configuration(object):
 	DEBUG = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:q@localhost/my_db'
-	config['SECRET_KEY'] = '503015013694b169f37690a490978479653b'
+	SECRET_KEY = '503015013694b169f37690a490978479653b'
