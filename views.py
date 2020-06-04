fdda93ebc58af4afa55ef1bc49a4d1dbf781b7a7
SuPeRBoY321 <muso-azizbekov@mail.ru>
2020-06-05 02:05:21 +0300

    Update views.py

--- views.py
+++ views.py
@@ -17,4 +17,4 @@ def register():
 @app.route("/login")
 def login():
 	form = LoginForm()
-	return render_template('Login.html', title='Login', form=form)
+	return render_template('login.html', title='Login', form=form)
