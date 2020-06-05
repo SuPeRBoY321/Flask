f4e7330c1171b0ce833b9aa91dd6685156d07f7f
SuPeRBoY321 <muso-azizbekov@mail.ru>
2020-06-05 17:29:03 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from flask import flash
+from flask import url_for
 from models import Post
 from login import RegistrationForm
 from login import LoginForm
@@ -9,9 +11,12 @@ from login import LoginForm
 def index():
 	return render_template('default.html') 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created of {form.username.date}!', 'success')
+		return redirect(url_for('index'))
 	return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
